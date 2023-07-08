2106809e7c5eed20d1b21a3cdde27f3c4dc19b4b
Ольга Ротбардт <knyajna.tarakanova@gmail.com>
2023-07-08 20:06:26 +0400

    apartment coordinates

--- matroskin/spiders/realting_spider.py
+++ matroskin/spiders/realting_spider.py
@@ -1,3 +1,5 @@
+import re
+
 import scrapy
 
 from scrapy.utils.project import get_project_settings
@@ -21,11 +23,34 @@ class RealtingSpider(scrapy.Spider):
         def extract_with_css(query):
             return response.css(query).get(default='').strip()
 
-        yield {
+        result = {
             'url': response.url,
             'summary': extract_with_css('h1::text'),
             'location': extract_with_css('.loc > a::attr(onclick)').split("'")[1],
-            'price': extract_with_css('.price > span::text'),
-            'about': dict(zip(response.css('.t::text').getall(),
-                              response.css('.i::text').getall())),
+            'price': extract_with_css('.price > span::text')
         }
+
+        result.update(dict(zip(response.css('.t::text').getall(),
+                               response.css('.i::text').getall())))
+
+        map_link = extract_with_css('.loc > a::attr(onclick)').split("'")[3]
+
+        yield response.follow(map_link,
+                              self.parse_coordinates,
+                              meta={'result': result})
+
+    def parse_coordinates(self, response):
+        def extract_with_css(query):
+            return response.css(query).get(default='').strip()
+
+        coordinates = re.search(r"\[([\d.,]+)\]",
+                                extract_with_css('script::text')).group(1)
+
+        coordinates_list = [float(coord) for coord in coordinates.split(",")]
+
+        result = response.meta['result']
+
+        result['latitude'] = coordinates_list[0]
+        result['longitude'] = coordinates_list[1]
+
+        yield result
