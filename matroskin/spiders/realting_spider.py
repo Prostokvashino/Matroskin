035dc18d8144c19f00a48697b2cd0aa5ed2c1b56
Ольга Ротбардт <knyajna.tarakanova@gmail.com>
2023-07-09 18:27:10 +0400

    fix pagination, add posted and updated dates

--- matroskin/spiders/realting_spider.py
+++ matroskin/spiders/realting_spider.py
@@ -2,33 +2,45 @@ import re
 
 import scrapy
 
-from scrapy.utils.project import get_project_settings
-
-
-settings = get_project_settings()
-
 
 class RealtingSpider(scrapy.Spider):
     name = 'realting'
-    start_urls = [settings.get('START_URL')]
+
+    def start_requests(self):
+        return [
+            scrapy.Request(
+                self.settings.get('START_URL'),
+                callback=self.parse,
+            )
+        ]
 
     def parse(self, response):
         apartment_page_links = response.css('.gl > a::attr(href)').extract()
         yield from response.follow_all(apartment_page_links,
                                        self.parse_apartment)
-        pagination_links = response.css('.dlf a')
-        yield from response.follow_all(pagination_links, self.parse)
+        pagination_link = response.css('.dlf > span + a::attr(href)').get()
+        if pagination_link:
+            yield response.follow(pagination_link, self.parse)
 
     def parse_apartment(self, response):
         def extract_with_css(query):
             return response.css(query).get(default='').strip()
+
+        date_time = ''
+        spans = response.css('.footer span::text')
+        if len(spans) > 2:
+            match = re.search(r"(\d{2}\.\d{2}\.\d{4} \d{2}:\d{2})",
+                              spans[2].get())
+            date_time = match.group(1)
 
         result = {
             'id': response.url.split('/')[-1],
             'url': response.url,
             'summary': extract_with_css('h1::text'),
             'location': extract_with_css('.loc > a::attr(onclick)').split("'")[1],
-            'price': extract_with_css('.price > span::text')
+            'price': extract_with_css('.price > span::text'),
+            'posted_at': extract_with_css('.footer span::attr(content)'),
+            'updated_at': date_time,
         }
 
         result.update(dict(zip(response.css('.t::text').getall(),
